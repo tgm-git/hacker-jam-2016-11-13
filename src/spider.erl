5d58dd8eead11f6881a4b110ae3a249bf23e42a5
electric coffee <slench102@gmail.com>
2016-11-19 13:37:09 +0100

    move link_repo away from spider.erl

--- src/spider.erl
+++ src/spider.erl
@@ -57,21 +57,3 @@ store_links(Links, C) when is_record(C, context) ->
         fun(Link) -> C#context.repo ! {store, Link} end,
     lists:foreach(Callback, Links),
     ok.
-
-%% @doc stores the sites visited so that a page isn't visited twice.
-%% recursive calls to itself ensures that it stays alive, 
-%% while acting like a mutable store
-%% @TODO Create CRUD messages
-link_repo(Links) ->
-    receive
-        {From, {find, URL, Ref}} ->             % synchronous message
-            case lists:member(URL, Links) of
-                true  -> From ! {hit,  Ref};
-                false -> From ! {miss, Ref}
-            end,
-            link_repo(Links);
-        {store, URL} ->                         % asynchronous message
-            link_repo([URL | Links]);
-        terminate -> ok                         % termination message
-    end.
-
