0fb7703392537fa5e4ba80387f77b1197dd89af3
Niko Lepka <slench102+git@gmail.com>
2016-12-01 17:38:30 +0100

    add gen_server functions

--- src/repo.erl
+++ src/repo.erl
@@ -1,32 +1,46 @@
 -module(repo).
 -compile(export_all).
 -behaviour(gen_server).
-%% @doc stores the sites visited so that a page isn't visited twice.
-%% recursive calls to itself ensures that it stays alive, 
-%% while acting like a mutable store
-%% @TODO Create CRUD messages
-link_repo(Links) ->
-    receive
-        {From, {find, URL, Ref}} ->             % synchronous message
-            case lists:member(URL, Links) of
-                true  -> From ! {hit,  Ref};
-                false -> From ! {miss, Ref}
-            end,
-            link_repo(Links);
-        {store, URL} ->                         % asynchronous message
-            link_repo([URL | Links]);
-        terminate -> ok                         % termination message
-    end.
+
+%% REPO FUNCTIONS
+start() ->
+    gen_server:start(?MODULE, [], []).
+
+start_link() ->
+    gen_server:start_link(?MODULE, [], []).
+
+create_url(Pid, URL) ->
+    gen_server:cast(Pid, {create, URL}).
+
+read_url(Pid, URL) ->
+    gen_server:call(Pid, {read, URL}).
+
+delete_url(Pid, URL) ->
+    gen_server:cast(Pid, {delete, URL}).
+
+drop(Pid) ->
+    gen_server:cast(Pid, drop_repo).
+
+%% GEN_SERVER CALLBACK FUNCTIONS
 init(_Args) -> ignore.
 
 handle_info(_Info, _State) -> {noreply, _State}.
 
-handle_cast(_Request, _State) -> {noreply, _State}.
+% handle_cast(_Request, _State) -> {noreply, _State}.
+handle_cast({create, URL}, Links) ->
+    {noreply, [URL | Links]};
+handle_cast({delete, URL}, Links) ->
+    {noreply, [Link || Link <- Links, Link =/= URL]};
+handle_cast(drop_repo, _Links) ->
+    {noreply, []}.
 
-handle_call(_Request, _From, _State) -> {noreply, _State}.
-handle_call({read, URL}, _From, Links) -> .
+
+handle_call({read, URL}, _From, Links) -> 
+    case lists:member(URL, Links) of
+        true -> {reply, hit};
+        false -> {reply, miss}
+    end.
 
 code_change(_OldVsn, _State, _Extra) -> ok.
 
 terminate(_Reason, _State) -> ok.
-
