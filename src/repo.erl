182356afbec9d9e4883b0631e3dccc1489bd4133
tgm <tgmmail@hush.com>
2016-12-01 16:40:07 +0100

    Add empty function definitions.

--- src/repo.erl
+++ src/repo.erl
@@ -1,6 +1,6 @@
 -module(repo).
 -compile(export_all).
-
+-behaviour(gen_server).
 %% @doc stores the sites visited so that a page isn't visited twice.
 %% recursive calls to itself ensures that it stays alive, 
 %% while acting like a mutable store
@@ -17,4 +17,10 @@ link_repo(Links) ->
             link_repo([URL | Links]);
         terminate -> ok                         % termination message
     end.
+init(_foo).
+handle_info(_bar, _baz).
+handle_cast(_x, _y).
+handle_call(_z, _q, _w).
+code_change(_e, _r, _t).
+terminate(_sleep, _tight).
 
